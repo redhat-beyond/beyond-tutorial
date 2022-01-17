e59a439c7e5f4a1ab162e99a0e65fc8ad2f4be99
Barak Korren <bkorren@redhat.com>
2022-01-17 18:11:31 +0200

    Add accounts and logins
    Adding "Account" objects that extend the built-in User objects. Mostly
    following the procedure here:
    https://docs.djangoproject.com/en/4.0/topics/auth/customizing/#extending-the-existing-user-model
    
    Based on those account objects added:
    - Login button
    - A login screen
    - Logout button
    - User display on the nav bar

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def login_form(request):
+    context = {}
+    if settings.DEBUG:
+        if 'du' in request.GET:
+            try:
+                user = User.objects.get_by_natural_key(request.GET['du'])
+                login(request, user)
+                return redirect(request.GET['next'])
+            except User.DoesNotExist:
+                return redirect('login')
+        context['dev_accounts'] = User.objects.all()
+    return render(request, 'accounts/login.html', context)
--- beyond_tutorial/urls.py
+++ beyond_tutorial/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path
-from msgboard import views
+from msgboard.views import board
+from accounts.views import login_form  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.board, name='board')
+    path('', board, name='board'),
+    path('accounts/login/', login_form, name="login"),
+    path('accounts/logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
